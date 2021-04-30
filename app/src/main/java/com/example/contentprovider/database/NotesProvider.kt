2cb696b65845ee0f46e7fe59d41d2aa4a96e5de0
Bianca Barbosa <yume_shiro@hotmail.com>
2021-04-30 20:00:07 -0300

    imp insert, select and update content provider

--- app/src/main/java/com/example/contentprovider/database/NotesProvider.kt
+++ app/src/main/java/com/example/contentprovider/database/NotesProvider.kt
@@ -45,45 +45,111 @@ class NotesProvider : ContentProvider() {
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
         //sempre verificar a URL antes
         if (mUriMatcher.match(uri) == NOTES_BY_ID) {
+            //operação de delete
             val db: SQLiteDatabase? = dbHelper.writableDatabase //habilitado para mexer
-            val linesAffect = db?.delete(TABLE_NOTES, "$_ID =?", arrayOf(uri.lastPathSegment))
+            val linesAffect: Int? = db?.delete(TABLE_NOTES, "$_ID =?", arrayOf(uri.lastPathSegment))
 
             db?.close()
+            //notifica sobre alteração no content provider
             context?.contentResolver?.notifyChange(uri, null)
 
-            if (linesAffect != null) {
-                return linesAffect
-            }
+             return linesAffect!!
+
+
         } else {
             throw UnsupportedSchemeException("Uri invalida para exclusao")
         }
     }
 
-    //valida uma URL
+    //valida uma URL, serve para manipulação de arquivos
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     override fun getType(uri: Uri): String? {
         throw UnsupportedSchemeException("Uri não implementada")
     }
 
     //insere dados no provider
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        //se for feita requisição de um valor
+        if (mUriMatcher.match(uri) == NOTES) {
+            val db: SQLiteDatabase? = dbHelper.writableDatabase //habilitado para mexer
+            val id = db?.insert(TABLE_NOTES, null, values)
+            val insertUri =
+                Uri.withAppendedPath(BASE_URI, id.toString()) //declada o uri e o insere no sqLite
+
+            db?.close()
+
+            context?.contentResolver?.notifyChange(uri, null) //notifica sobre a mudança
+            return insertUri
+
+        }else{
+            throw UnsupportedSchemeException("Uri inválida para inserção")
+        }
 
     }
 
         //seleciona os dados do provider, retorna um cursor
+        @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
         override fun query(
             uri: Uri, projection: Array<String>?, selection: String?,
             selectionArgs: Array<String>?, sortOrder: String?
         ): Cursor? {
-            TODO("Implement this to handle query requests from clients.")
+            return when {
+                mUriMatcher.match(uri) == NOTES -> {
+                    val db: SQLiteDatabase = dbHelper.writableDatabase
+                    //os dados são passados para um cursos
+                    val cursor = db.query(
+                        TABLE_NOTES,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                    )
+
+                    //notifica que recebeu os dados
+                    cursor.setNotificationUri(context?.contentResolver, uri)
+                    cursor
+
+                }
+                mUriMatcher.match(uri) == NOTES_BY_ID -> {
+                    val db: SQLiteDatabase = dbHelper.writableDatabase
+                    val cursor = db.query(
+                        TABLE_NOTES,
+                        projection,
+                        "$_ID = ?",
+                        arrayOf(uri.lastPathSegment),
+                        null,
+                        null,
+                        sortOrder
+                    )
+                    cursor.setNotificationUri(context?.contentResolver, uri)
+                    cursor
+                }
+                else -> {
+                    throw UnsupportedSchemeException("Uri não implementada")
+                }
+            }
         }
 
         //atualiza os dados do providades
+        @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
         override fun update(
             uri: Uri, values: ContentValues?, selection: String?,
             selectionArgs: Array<String>?
         ): Int {
-            TODO("Implement this to handle requests to update one or more rows.")
+            if (mUriMatcher.match(uri) == NOTES_BY_ID) {
+                val db: SQLiteDatabase = dbHelper.writableDatabase
+                val linesAffect = db.update(TABLE_NOTES, values, "$_ID = ?", arrayOf(uri.lastPathSegment))
+
+                db.close()
+
+                context?.contentResolver?.notifyChange(uri, null)
+                return linesAffect
+            }  else {
+                throw UnsupportedSchemeException("Uri não implementada")
+            }
         }
 
         companion object {
@@ -98,4 +164,3 @@ class NotesProvider : ContentProvider() {
             const val NOTES_BY_ID = 2
         }
     }
-}
