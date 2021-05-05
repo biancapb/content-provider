0a3cf4ff242376becc26aabff7d39d440030ea4c
Bianca Barbosa <yume_shiro@hotmail.com>
2021-05-05 04:50:10 -0300

    creating notes detail

--- app/src/main/java/com/example/contentprovider/MainActivity.kt
+++ app/src/main/java/com/example/contentprovider/MainActivity.kt
@@ -1,11 +1,78 @@
 package com.example.contentprovider
 
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.BaseColumns._ID
+import androidx.annotation.RequiresApi
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.contentprovider.database.NotesDatabaseHelper.Companion.TITLE_NOTES
+import com.example.contentprovider.database.NotesProvider.Companion.URI_NOTES
+import com.example.contentprovider.database.NotesProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> {
+    lateinit var noteRecyclerView: RecyclerView
+    lateinit var noteAdd: FloatingActionButton
+
+    lateinit var adapter: NotesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        noteAdd = findViewById(R.id.notes_add)
+        noteAdd.setOnClickListener {
+            NotesDetailFragment().show(supportFragmentManager, "dialog")
+        }
+
+        adapter = NotesAdapter(object : NoteClickedListener {
+            override fun noteClickedItem(cursor: Cursor) {
+                val id: Long = cursor.getLong(cursor.getColumnIndex(_ID))
+                val fragment = NotesDetailFragment.newIntance(id)
+
+                //criação da interface para utilizar o supperFragmentManager, pois só se encontra na mainactivity
+                fragment.show(supportFragmentManager, "dialog")
+
+            }
+
+            override fun noteRemoveItem(cursor: Cursor) {
+                val id = cursor.getLong(cursor.getColumnIndex(_ID))
+                contentResolver.delete(Uri.withAppendedPath(URI_NOTES, id.toString()), null, null)
+            }
+
+        })
+        adapter.setHasStableIds(true)
+
+        noteRecyclerView = findViewById(R.id.notes_recycler)
+        noteRecyclerView.layoutManager = LinearLayoutManager(this)
+        noteRecyclerView.adapter = adapter
+
+        LoaderManager.getInstance(this).initLoader(0, null, this)
+
+    }
+
+    //serve para instanciar aquilo que for buscado no content provider
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        return CursorLoader(this, URI_NOTES, null, null, null, TITLE_NOTES)
+    }
+
+    //serve para pegar os dados inseridos
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        if (data != null) {
+            adapter.setCursos(data)
+        }
+    }
+
+    //acaba com a pesquisa de segundo plano do loadermanager
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        adapter.setCursos(null)
     }
 }
